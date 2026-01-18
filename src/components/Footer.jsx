c7d5f447a4820ab54ca09725e7b9a5ae8bd7fb8f
Umar790654 <umar790654@gmail.com>
2026-01-18 11:30:16 -0600

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export default function Footer(props) {
+    const { showModal, handleToggleModal, data } = props
+
+    return (
+        <footer>
+            <div className="bgGradient"></div>
+            <div>
+                <h3>APOD PROJECT</h3>
+                <h2>{data?.title}</h2>
+            </div>
+            <button onClick={handleToggleModal}>
+                <i className="fa-solid fa-circle-info"></i>
+            </button>
+        </footer>
+    )
+}
